2fc74453cbbde3e2a48bdaad0cfc1092065a909a
mohamadmorady412 <mohamadmorady412@gmail.com>
2025-04-10 13:34:48 +0330

    str

--- src/config_reader.py
+++ src/config_reader.py
@@ -1,10 +1,27 @@
 import yaml
 from pathlib import Path
+from logger import get_logger
 
-config_path = Path("../config/config.yaml")
+log = get_logger(__name__)
 
-def load_config():
-    with open(config_path, "r") as file:
-        config = yaml.safe_load(file)
-    
-    return config
+CONFIG_FILE_PATH = Path("../config/config.yaml")
+
+def load_config(config_file: Path = CONFIG_FILE_PATH) -> dict:
+    try:
+        with open(config_file, "r") as file:
+            config = yaml.safe_load(file)
+        log.info(f"پیکربندی با موفقیت از '{config_file}' بارگیری شد.")
+        return config
+    except FileNotFoundError:
+        error_message = f"فایل پیکربندی در مسیر '{config_file}' یافت نشد."
+        log.error(error_message)
+        raise FileNotFoundError(error_message)
+    except yaml.YAMLError as e:
+        error_message = f"خطا در هنگام بارگیری فایل YAML '{config_file}': {e}"
+        log.error(error_message)
+        raise yaml.YAMLError(error_message)
+    except Exception as e:
+        error_message = f"یک خطای غیرمنتظره در هنگام بارگیری پیکربندی رخ داد: {e}"
+        log.error(error_message)
+        raise
+
--- src/main.py
+++ src/main.py
@@ -1,5 +1,1 @@
 from config_reader import load_config
-
-
-config = load_config()
-print(f"{config["Try"]["NUM_TRIALS"]}")
